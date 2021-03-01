45667de536e606ebd1a0b3b25f20654fd4ab7755
Wes Parish <wes@elastiscale.net>
2021-03-01 02:16:24 +0000

    Initial creation

--- build.sh
+++ build.sh
@@ -1,33 +1,67 @@
 #!/bin/bash
 
-registry=${1:-wesparish}
-imagename=teamredminer
-imagename=${2:-$imagename}
+usage() { echo "Usage: $0 [-p (disable push)] " \
+               "[-r <registry> (default: wesparish)] " \
+               "[-i <image name> (default: $(basename $PWD))] " \
+               "[-a <additional private registry> (default: none)]" 1>&2; exit 1; }
 
-privateRegistry=${3:-nexus-jamie-docker.elastiscale.net}
+# Defaults
+push=true
+registry="wesparish"
+imagename="$(basename $PWD)"
+privateRegistry=
+echo "Args: push: $push, registry: $registry, imagename: $imagename, privateRegistry: $privateRegistry"
+
+while getopts ":pr:i:a:" o; do
+    case "${o}" in
+        p)
+            push=
+            ;;
+        r)
+            registry=${OPTARG}
+            ;;
+        i)
+            imagename=${OPTARG}
+            ;;
+        a)
+            pivateRegistry=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+echo "Args: push: $push, registry: $registry, imagename: $imagename, privateRegistry: $privateRegistry"
 
 buildDate=$(date +%Y-%m-%d-%H%M%S)
 
+set -e
+
 for dockerfile in $(find  -name Dockerfile); do
+  rsync -av --delete ./crypto-prometheus/ $(dirname $dockerfile)/crypto-prometheus/
   versionvariant=$(dirname $dockerfile | sed -e 's|^./||g' -e 's|/|-|g')
   echo Building variant: $versionvariant
   echo docker build -t $registry/${imagename}:${versionvariant} $(dirname $dockerfile)
   docker build -t $registry/${imagename}:$versionvariant $(dirname $dockerfile)
   echo docker push $registry/${imagename}:${versionvariant}
-  docker push $registry/${imagename}:$versionvariant
+  [ $push ] && docker push $registry/${imagename}:$versionvariant
 
   docker tag $registry/${imagename}:${versionvariant} $registry/${imagename}:${versionvariant}-${buildDate}
   echo docker push $registry/${imagename}:${versionvariant}-${buildDate}
-  docker push $registry/${imagename}:${versionvariant}-${buildDate}
+  [ $push ] && docker push $registry/${imagename}:${versionvariant}-${buildDate}
 
   if [ -n "$privateRegistry" ] ; then
     echo docker tag $registry/${imagename}:${versionvariant} ${privateRegistry}/${imagename}:${versionvariant}
     docker tag $registry/${imagename}:$versionvariant ${privateRegistry}/${imagename}:$versionvariant
     echo docker push ${privateRegistry}/${imagename}:${versionvariant}
-    docker push ${privateRegistry}/${imagename}:$versionvariant
+    [ $push ] && docker push ${privateRegistry}/${imagename}:$versionvariant
 
     docker tag $registry/${imagename}:${versionvariant} $registry/${imagename}:${versionvariant}-${buildDate}
     echo docker push $registry/${imagename}:${versionvariant}-${buildDate}
-    docker push $registry/${imagename}:${versionvariant}-${buildDate}
+    [ $push ] && docker push $registry/${imagename}:${versionvariant}-${buildDate}
   fi
 done
+
+set +e
